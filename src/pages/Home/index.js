ce9c0f97f2f7e1777235ae8ae11256ab0f74fd71
Chris Pucknell <chris.pucknell@gmail.com>
2022-10-05 13:15:29 +0100

    add links to home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import './style.css';
 
 const Home = () => {
-    return <h1>Home page</h1>
+    return (<>
+    <h1>Home page</h1>
+    <Link to="/Home"><Home/></Link>
+    <Link to="/GamePage"><Home/></Link>
+    <Link to="/KudosPage"><Home/></Link>
+    <Link to="/SettingsPage"><Home/></Link>
+    </>)
+
+
 }
 
 export default Home;
