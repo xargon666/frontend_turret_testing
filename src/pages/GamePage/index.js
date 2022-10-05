4f1d027835487d9465c6e561637c2a866900d1b6
Chris Pucknell <chris.pucknell@gmail.com>
2022-10-05 13:31:21 +0100

    add links to home, remove gamePage feature added in error

--- src/pages/GamePage/index.js
+++ src/pages/GamePage/index.js
@@ -1,32 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './style.css';
 
 const GamePage = () => {
-
-    const [questionText, updateQuestion] = useState('placeholder text')
-    const [questionCounter, updateQuestionCounter] = useState(0)
-
-    useEffect(() => {
-        nextQuestion();
-    }, [])
-
-    const nextQuestion = async () => {
-        let questionIndex = questionCounter + 1
-        updateQuestionCounter(questionCounter + 1)
-        const res = fetch(`https://jsonplaceholder.typicode.com/posts/${questionIndex}`)
-        updateQuestion(await res.json())
-    }
-
-    return (
-        <>
-            <h1>I am the Game page!</h1>
-            <span>{questionText}</span>
-            <button
-                className='next-question-button'
-                onClick={() => nextQuestion()}
-            ></button>
-        </>
-    )
+    return <h1>I am the Game page!</h1>
 }
 
 export default GamePage;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './style.css';
-
+import { Link } from 'react-router-dom';
 const Home = () => {
-    return (<>
-    <h1>Home page</h1>
-    <Link to="/Home"><Home/></Link>
-    <Link to="/GamePage"><Home/></Link>
-    <Link to="/KudosPage"><Home/></Link>
-    <Link to="/SettingsPage"><Home/></Link>
-    </>)
-
-
+    return (
+    <>
+        <h1>Home page</h1>
+        <Link to="/">Home</Link>
+        <Link to="/GamePage">Game Page</Link>
+        <Link to="/KudosPage">Kudos Page</Link>
+        <Link to="/SettingsPage">Settings Page</Link>
+    </>
+    )
 }
 
 export default Home;
