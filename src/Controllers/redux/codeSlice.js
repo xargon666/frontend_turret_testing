e1df33b064f9a0affd55b55774b83924d2486bd6
sigltech <101203107+sigltech@users.noreply.github.com>
2022-11-02 12:11:22 +0000

    added redux controllers and actions

---
+++ src/Controllers/redux/codeSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+export const codeSlice = createSlice({
+    name: "code",
+    initialState: {
+        value: `// starting code...`,
+        isLoading: false,
+        error: null
+    },
+    reducers: {
+        setCode: (state, action) => {
+            state.value = action.payload;
+        }
+    }
+});
+
+export const { setCode } = codeSlice.actions;
+
+export default codeSlice.reducer;
