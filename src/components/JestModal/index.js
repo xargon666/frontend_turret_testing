b10937ccaf103d91dbee36f175453ad63e68e2e4
mariesophiecd <mariesophie.cduran@gmail.com>
2022-11-25 15:05:07 +0000

    added styling to JestModal component

--- src/components/JestModal/index.js
+++ src/components/JestModal/index.js
@@ -10,8 +10,8 @@ const JestModal = ({open, onClose}) => {
                 <div className='modal-container' onClick={(e) => e.stopPropagation()}>
                     <p onClick={onClose} className='close-btn'>X</p>
                     <div className='modal-content'>
-                        <h1 className='modal-h1'>Intro to Jest</h1>
-                        <h2>What you should know before you start</h2>
+                        <h1>Jest 1.0.1</h1>
+                        <h2>Getting started !</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
                 </div>
