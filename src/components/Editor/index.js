687d16e3e28b0f50f9228a7f8871beb7632480c0
sigltech <101203107+sigltech@users.noreply.github.com>
2022-11-29 20:44:41 +0000

    adds turnery statement to check for props.code or props.function for the editors value prop

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,13 +1,11 @@
 import React, { useState, useRef } from "react";
 // import ReactDOM from "react-dom";
 import Editor from "@monaco-editor/react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setCode } from "../../Controllers/redux/codeSlice";
 
 function TextEditor(props) {
   const dispatch = useDispatch();
-  // const [content, setContent] = useState("// some code");
-  const question = useSelector((state) => state.question.data);  
 
   function handleEditorChange(value, event) {
     dispatch(setCode(value));
@@ -23,7 +21,7 @@ function TextEditor(props) {
         }}
         defaultLanguage="javascript"
         // defaultValue={props.code}
-        value={props.code}
+        value={props.code ? props.code : props.function}
         onChange={handleEditorChange}
       />
     </>
