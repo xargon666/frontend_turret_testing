6d4c67ab8d148b5ebb86cd8388dffc3be185d08b
sigltech <101203107+sigltech@users.noreply.github.com>
2022-11-30 22:38:05 +0000

    added a state for language and difficulty query parameters

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Home, SettingsPage, GamePage, KudosPage } from './pages';
 import './index.css';
 
 
 function App() {
+
+
+  // API query to get questions from database with language and difficulty level
+
+  const [lang, setLang] = useState("javascript");
+  const [difficulty, setDifficulty] = useState("easy");
+
   return (
     <>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/settings' element={<SettingsPage />} />
-        <Route path='/game' element={<GamePage />} />
+        <Route path='/settings' element={<SettingsPage lang={lang} setLang={setLang} difficulty={difficulty} setDifficulty={setDifficulty} />} />
+        <Route path='/game' element={<GamePage lang={lang} setLang={setLang} difficulty={difficulty} setDifficulty={setDifficulty} />} />
         <Route path='/kudos' element={<KudosPage />} />
       </Routes>
     </>
